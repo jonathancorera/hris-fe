5e8c1f8d802559584f3395cfdf5c738d3c7a84cf
jonathanc <jonathanc@mexxar.com>
2022-08-26 11:04:26 +0530

    updated login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,13 +4,13 @@ import LoginCardForm from "../components/LoginCardForm";
 
 function LoginPage () {
     return(
-    <div>
+
     <div class="flex mb-4">
     <div class="w-1/3 bg-gray-400 h-screen"></div>
     <div class="w-2/3 bg-gray-500 h-screen"></div>
     <LoginCardForm />
     </div>
-    </div>
+   
     );
 
 }
